755c3280c1b9ae390ff9185300f47e3f45325c69
Martten Mitri <martten@tlu.ee>
2023-01-28 19:17:39 +0200

    Martten.com
    *First edition of martten.com

---
+++ script.js
@@ -0,0 +1,8 @@
+function fade(){
+    let nametag = document.getElementById("nametag");
+    let nametag_white = document.getElementById("nametag-white");
+
+    nametag.classList.toggle('fade');
+    nametag_white.classList.toggle('fade');
+
+    }
